9696da0b1c1a94b075ab0490210cb0069c83f45e
kaj-al <kt2519033@gmail.com>
2026-03-08 20:42:31 +0530

    change the prompt file

--- prompt.py
+++ prompt.py
@@ -1,25 +1,43 @@
-from langchain_core.prompts import ChatPromptTemplate
-from config import llm 
 
-
+from config import llm
+from langchain_community.prompts import ChatPromptTemplate, MessagesPlaceholder
 
 prompt = ChatPromptTemplate.from_messages([
-   ("system","You are a helpful AI teacher. Explain clearly in simple English and then language specified by user"),
-   ("placeholder","{history}"),
-   ("human","{input}\nLangauge:{language}")
+    (
+        "system",
+        """
+You are an AI Learning Coach.
+
+Process:
+1. Explain the concept simply.
+2. Give a small real-world example.
+3. Ask ONE multiple choice quiz question.
+4. Wait for student answer.
+5. If correct → appreciate and give slightly harder question.
+6. If wrong → explain again simply.
+
+Always respond in the selected language.
+"""
+    ),
+
+    MessagesPlaceholder("chat_history"),
+
+    ("human", "Topic or answer: {input}\nLanguage: {language}")
 ])
 
 chain = prompt | llm
 
-def response(input,language,memory):
+
+def get_response(user_input, language, memory):
+
     response = chain.invoke({
-        "input":input,
-        "language":language,
-        "history":memory.history(),
+        "input": user_input,
+        "language": language,
+        "chat_history": memory.get_history()
     })
 
-    memory.add_user(input)
+    memory.add_user(user_input)
     memory.add_ai(response.content)
-    
+
     return response.content
 
